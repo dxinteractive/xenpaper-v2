4d1c8b2d47f395be3cf296e41c1af254eac4caba
Damien Clarke <dclarke@atlassian.com>
2022-11-27 19:31:56 +1100

    add minimum manifest config

--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -12,6 +12,30 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       selfDestroying: true,
+      manifest: {
+        name: "Xenpaper",
+        short_name: "Xenpaper",
+        description: "Text-based microtonal sequencer.",
+        theme_color: "#0e151b",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
     }),
   ],
 });
